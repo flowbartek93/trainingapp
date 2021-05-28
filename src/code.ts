0baa525232e86812a3df55c423a3936d83849b89
Bartek <submission93@gmail.com>
2021-05-28 12:31:05 +0200

    first commit

---
+++ src/code.ts
@@ -0,0 +1,95 @@
+//DOM
+
+const milsecsContainer = document.getElementById("timer__milsecs")!;
+const secsContainer = document.getElementById("timer__secs")!;
+const minsContainer = document.getElementById("timer__mins")!;
+
+//Buttons
+const startBtn = document.querySelector(".startBtn")!;
+const stopBtn = document.querySelector(".stopBtn")!;
+let isCounting: boolean | null = null;
+
+let timerID: number | NodeJS.Timeout | null;
+
+//Types
+type timerData = {
+  milsecs: number;
+  secs: number;
+  mins: number;
+};
+
+type timerSettings = {
+  durationMinutes?: number;
+  durationSecs?: number;
+  rounds?: number;
+  rest?: number;
+};
+
+const timer = (settings?: timerSettings, data?: timerData) => {
+  let milsecs: number;
+  let secs: number;
+  let mins: number;
+
+  if (data) {
+    milsecs = data.milsecs;
+    secs = data.secs;
+    mins = data.mins;
+  } else {
+    milsecs = 0;
+    secs = 0;
+    mins = 0;
+  }
+
+  const startCount = setInterval(() => {
+    if (settings) {
+      if (secs === settings.durationSecs) {
+        console.log(milsecs);
+        clearInterval(startCount);
+      }
+    }
+
+    if (milsecs >= 100) {
+      milsecs = 0;
+      console.log(milsecs);
+      secs++;
+    } else if (milsecs < 100) {
+      milsecs++; // counting from milsecs
+    }
+
+    if (secs >= 60) {
+      secs = 0;
+      mins++;
+    }
+
+    milsecsContainer.textContent = milsecs === 1 && secs > 1 ? "00" : milsecs.toString();
+    secsContainer.textContent = secs < 10 ? "0" + secs.toString() : secs.toString();
+    minsContainer.textContent = mins < 10 ? "0" + mins.toString() : mins.toString();
+  }, 1);
+
+  return { startCount, milsecs, secs, mins };
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  startBtn.addEventListener("click", (e: Event) => {
+    const button = e.target as HTMLButtonElement;
+
+    if (button.classList.contains("active")) {
+      button.classList.remove("active");
+      startBtn.textContent = "Start";
+      if (timerID) {
+        if (typeof timerID === "number") {
+          clearInterval(timerID);
+        }
+      }
+    } else {
+      button.classList.add("active");
+      startBtn.textContent = "Pause";
+      const { startCount, milsecs, secs, mins } = timer();
+      timerID = startCount;
+
+      console.log("start");
+    }
+  });
+});
+
+// timer({ durationSecs: 10 });
